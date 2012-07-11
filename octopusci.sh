8d30a586b72c8b23ed9a8013598b6f1bbbc686c9
Andrew De Ponte <cyphactor@gmail.com>
2012-07-10 22:59:29 -0700

    Add some bash scripts to setup tmux sessions.
    I reworked the rpp_web_app.sh script to be cleaner and to handle
    checking for existance of the session before creating it and attaching
    if it already exists. I used that pattern for the other tmux session
    startup scripts as well. I added a script for my personal blog and for
    octopusci.

--- octopusci.sh
+++ octopusci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TMUX_SESSION_NAME="rpp_web_app"
+TMUX_SESSION_NAME="octopusci"
 
 tmux list-sessions | grep ${TMUX_SESSION_NAME}
 if [ $? -eq 0 ]; then
@@ -12,7 +12,7 @@ else
   echo "The \"${TMUX_SESSION_NAME}\" tmux session does NOT exist. Creating it..."
 
   # Prime the CWD so that it is used as the CWD for the session and its windows
-  cd /Users/adeponte/WebApps/realpractice/rpp_web_app
+  cd /Users/adeponte/Documents/programming/sandbox/octopusci
 
   # Create a new session.
   tmux new-session -s "${TMUX_SESSION_NAME}" -n vim -d '/bin/zsh'
@@ -20,19 +20,9 @@ else
   # Create a new window inside the session called 'vim' containing an instance of vim.
   tmux send-keys -t "${TMUX_SESSION_NAME}:0" 'vim' C-m
 
-  # Create a new window inside the session called 'log' containing the apps dev logs.
-  tmux new-window -t "${TMUX_SESSION_NAME}" -n log -d 'tail -f log/development.log'
-
   # Create a new window inside the session called 'foreman' running 'foreman start'.
-  tmux new-window -t "${TMUX_SESSION_NAME}" -n foreman -d '/bin/zsh'
-  tmux send-keys -t "${TMUX_SESSION_NAME}:2" 'foreman start' C-m
-
-  # Create a new window inside the session called 'console' running 'rails c'.
-  tmux new-window -t "${TMUX_SESSION_NAME}" -n console -d '/bin/zsh'
-  tmux send-keys -t "${TMUX_SESSION_NAME}:3" 'rails c' C-m
-
-  # Create a new window inside the session called 'irb' running 'irb'.
-  tmux new-window -t "${TMUX_SESSION_NAME}" -n irb -d 'irb'
+  tmux new-window -t "${TMUX_SESSION_NAME}" -n rackup -d '/bin/zsh'
+  tmux send-keys -t "${TMUX_SESSION_NAME}:1" 'rackup -p 9000' C-m
 
   # Select the first window in the session for good measure.
   tmux select-window -t "${TMUX_SESSION_NAME}:0"
