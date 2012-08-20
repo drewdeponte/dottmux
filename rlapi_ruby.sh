78fb01d02f144ef5455b0996cf66db72f4d1a4f5
Andrew De Ponte <cyphactor@gmail.com>
2012-08-20 14:19:34 -0700

    Added the rlapi_ruby setup script.

---
+++ rlapi_ruby.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+TMUX_SESSION_NAME="rlapi_ruby"
+
+tmux list-sessions | grep ${TMUX_SESSION_NAME}
+if [ $? -eq 0 ]; then
+  echo "The \"${TMUX_SESSION_NAME}\" tmux session already exists. Attaching to it..."
+
+  # Attach the terminal to the session.
+  tmux attach-session -d -t "${TMUX_SESSION_NAME}"
+else
+  echo "The \"${TMUX_SESSION_NAME}\" tmux session does NOT exist. Creating it..."
+
+  # Prime the CWD so that it is used as the CWD for the session and its windows
+  cd /Users/adeponte/Documents/programming/realpractice/sandbox/rlapi_ruby
+
+  # Create a new session.
+  tmux new-session -s "${TMUX_SESSION_NAME}" -n vim -d '/bin/zsh'
+
+  # Create a new window inside the session called 'vim' containing an instance of vim.
+  tmux send-keys -t "${TMUX_SESSION_NAME}:0" 'vim' C-m
+
+  # Create a new window inside the session called 'irb' running 'irb'.
+  tmux new-window -t "${TMUX_SESSION_NAME}" -n irb -d '/bin/zsh'
+  tmux send-keys -t "${TMUX_SESSION_NAME}:3" 'irb' C-m
+
+  # Create a new window inside the session called 'console' running 'rails c'.
+  tmux new-window -t "${TMUX_SESSION_NAME}" -n console -d '/bin/zsh'
+
+  # Select the first window in the session for good measure.
+  tmux select-window -t "${TMUX_SESSION_NAME}:0"
+
+  # Attach the terminal to the session.
+  tmux attach-session -d -t "${TMUX_SESSION_NAME}"
+fi
+
